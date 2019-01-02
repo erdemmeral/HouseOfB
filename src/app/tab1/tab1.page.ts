e2605007f852d507dc2da6f4469aee6a461a1ebc
erdemmeral <erdem_meral@hotmail.com>
2019-01-02 18:32:26 +0300

    home page improvement

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+  constructor(public nav: NavController){
+
+  }
+  yemeksepeti() {
+    window.location.href = "https://www.yemeksepeti.com/house-of-b-besiktas-sinanpasa-mah-istanbul";
+  }
+  tel(){
+    window.open("tel:+902122362747");
+  }
+
+  
+}
